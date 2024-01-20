8cfa8f493b2d8599ea50f8c1eaed4e445662fac3
joaoaziliero <146762748+joaoaziliero@users.noreply.github.com>
2024-01-20 17:45:27 -0300

    add player speed to the game parameters

--- Assets/Scripts/GameplayParameters.cs
+++ Assets/Scripts/GameplayParameters.cs
@@ -7,12 +7,16 @@ using System;
 public class GameplayParameters : ScriptableObject
 {
     public Func<Vector2> BallSpeed { get; }
+    public Func<float> PlayerPaddleSpeed { get; }
     
     [Header("Ball speed")]
     [SerializeField] private Vector2 _ballSpeed;
+    [Header("Player speed")]
+    [SerializeField] private float _playerPaddleSpeed;
 
     private GameplayParameters()
     {
         BallSpeed = () => new Vector2(Mathf.Abs(_ballSpeed.x), Mathf.Abs(_ballSpeed.y));
+        PlayerPaddleSpeed = () => Mathf.Abs(_playerPaddleSpeed);
     }
 }
