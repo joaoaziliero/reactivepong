a83acaa84a5957d84b2b6d6862cd51d03d0e9117
joaoaziliero <146762748+joaoaziliero@users.noreply.github.com>
2024-01-21 20:29:23 -0300

    add ball speed multiplier to the gameplay parameters

--- Assets/Scripts/GameplayParameters.cs
+++ Assets/Scripts/GameplayParameters.cs
@@ -7,16 +7,20 @@ using System;
 public class GameplayParameters : ScriptableObject
 {
     public Func<Vector2> BallSpeed { get; }
+    public Func<float> BallSM { get; }
     public Func<float> PlayerPaddleSpeed { get; }
     
     [Header("Ball speed")]
     [SerializeField] private Vector2 _ballSpeed;
+    [Header("Ball speed multiplier")]
+    [SerializeField] private float _ballSM;
     [Header("Player speed")]
     [SerializeField] private float _playerPaddleSpeed;
 
     private GameplayParameters()
     {
         BallSpeed = () => new Vector2(Mathf.Abs(_ballSpeed.x), Mathf.Abs(_ballSpeed.y));
+        BallSM = () => _ballSM;
         PlayerPaddleSpeed = () => Mathf.Abs(_playerPaddleSpeed);
     }
 }
