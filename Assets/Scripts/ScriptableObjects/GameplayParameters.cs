674555401cd81ca012514ca4c288dba35dd096e1
joaoaziliero <146762748+joaoaziliero@users.noreply.github.com>
2024-01-22 13:54:55 -0300

    update folder structure

--- Assets/Scripts/ScriptableObjects/GameplayParameters.cs
+++ Assets/Scripts/ScriptableObjects/GameplayParameters.cs
@@ -9,6 +9,7 @@ public class GameplayParameters : ScriptableObject
     public Func<Vector2> BallSpeed { get; }
     public Func<float> BallSM { get; }
     public Func<float> PlayerPaddleSpeed { get; }
+    public Func<float> EnemyPaddleSpeed { get; }
     
     [Header("Ball speed")]
     [SerializeField] private Vector2 _ballSpeed;
@@ -16,11 +17,14 @@ public class GameplayParameters : ScriptableObject
     [SerializeField] private float _ballSM;
     [Header("Player speed")]
     [SerializeField] private float _playerPaddleSpeed;
+    [Header("Enemy speed")]
+    [SerializeField] private float _enemyPaddleSpeed;
 
     private GameplayParameters()
     {
         BallSpeed = () => new Vector2(Mathf.Abs(_ballSpeed.x), Mathf.Abs(_ballSpeed.y));
         BallSM = () => _ballSM;
         PlayerPaddleSpeed = () => Mathf.Abs(_playerPaddleSpeed);
+        EnemyPaddleSpeed = () => Mathf.Abs(_enemyPaddleSpeed);
     }
 }
