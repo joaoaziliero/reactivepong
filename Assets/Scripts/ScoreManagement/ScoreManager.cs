32ae331736b961875e5745a03408a37c93183007
joaoaziliero <146762748+joaoaziliero@users.noreply.github.com>
2024-01-22 19:32:55 -0300

    test score manager

--- Assets/Scripts/ScoreManagement/ScoreManager.cs
+++ Assets/Scripts/ScoreManagement/ScoreManager.cs
@@ -1,10 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UniRx;
 using UniRx.Triggers;
 using System;
 using System.Linq;
+using UnityEngine.SceneManagement;
 
 public class ScoreManager : MonoBehaviour
 {
@@ -12,7 +14,11 @@ public class ScoreManager : MonoBehaviour
     private Collider2D _collider;
 
     [Header("Gameplay parameters")]
-    [SerializeField] GameplayParameters _parameters;
+    [SerializeField] private GameplayParameters _parameters;
+    [Header("Corresponding display field")]
+    [SerializeField] private TextMeshProUGUI _textField;
+    [Header("Opponent's display field")]
+    [SerializeField] private TextMeshProUGUI _opposingTextField;
 
     private void Awake()
     {
@@ -27,35 +33,51 @@ public class ScoreManager : MonoBehaviour
 
     private void Start()
     {
-        TrackScore(_collider, _parameters.MaxPoints.Invoke()).AddTo(_compositeDisposable);
+        TrackScore(_collider, _parameters.MaxPoints.Invoke(), _textField, _opposingTextField)
+            .AddTo(_compositeDisposable);
     }
 
-    private IDisposable TrackScore(Collider2D thisCollider, int maxPoints)
+    private IDisposable TrackScore(Collider2D thisCollider, int maxPoints, TextMeshProUGUI textField, TextMeshProUGUI opposingField)
     {
-        GameObject ball = null;
-
-        var obs = thisCollider.OnTriggerEnter2DAsObservable()
-            .Where(thatCollider => thatCollider.gameObject.CompareTag("Ball"));
-
-        obs.Subscribe(thatCollider => { ball = thatCollider.gameObject; });
-
-        return obs.Select(collision => 1)
-            .Scan(0, (acc, current) => acc + current)
-            .Select<int, Action>(totalScore =>
+        return thisCollider.OnTriggerEnter2DAsObservable()
+            .Where(thatCollider => thatCollider.gameObject.CompareTag("Ball"))
+            .Select(collision => 1)
+            .Scan((id: 0, acc: 0), (tuple, increment) =>
             {
+                var totalScore = tuple.acc + increment;
+
                 return totalScore switch
                 {
-                    var value when value == maxPoints => () => { }
-                    ,
-                    _ => () => { }
-                    ,
+                    var value when value == maxPoints => (1, 0),
+                    _ => (0, totalScore),
                 };
             })
-            .Subscribe();
+            .Select<(int id, int acc), Action>(tuple =>
+            {
+                return tuple.id switch
+                {
+                    1 => () =>
+                    {
+                        textField.text = maxPoints.ToString();
+                        opposingField.text = (0).ToString();
+                        StartCoroutine(ReloadGame());
+                    }
+                    ,
+                    _ => () => { textField.text = tuple.acc.ToString(); }
+                };
+            })
+            .Subscribe(action => action.Invoke());
+    }
+
+    private IEnumerator ReloadGame(string sceneName = "SCN_Main", float timeBeforeReload = 1)
+    {
+        yield return new WaitForSeconds(timeBeforeReload);
+        SceneManager.LoadScene(sceneName);
     }
 
     private void OnDestroy()
     {
+        StopCoroutine(ReloadGame());
         _compositeDisposable.Dispose();
     }
 }
