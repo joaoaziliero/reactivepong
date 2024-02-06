9b5db6c80864fcc17c4ab9ab9137d7b7c46c2635
joaoaziliero <146762748+joaoaziliero@users.noreply.github.com>
2024-02-06 07:38:05 -0300

    implement vertical axis inputs for two players

--- Assets/Scripts/GameSettings/ScriptableObjects/SO_GameplayParameters.cs
+++ Assets/Scripts/GameSettings/ScriptableObjects/SO_GameplayParameters.cs
@@ -9,14 +9,19 @@ public class SO_GameplayParameters : ScriptableObject
     public Func<Vector2> BallSpeed { get; }
     public Func<float> BallSpeedMultiplier { get; }
     public Func<float> PlayerPaddleSpeed { get; }
+    public Func<string> LeftmostPlayerAxisName { get; }
+    public Func<string> RightmostPlayerAxisName { get; }
     public Func<int> MaxPoints { get; }
     
     [Header("Ball speed")]
     [SerializeField] private Vector2 _ballSpeed;
-    [Header("Ball speed multiplier")]
+    [Header("Ball speed multiplier (per collision)")]
     [SerializeField] private float _ballSpeedMultiplier;
     [Header("Player speed")]
     [SerializeField] private float _playerPaddleSpeed;
+    [Header("Input axis names for each player")]
+    [SerializeField] private string _leftmostPlayerAxisName;
+    [SerializeField] private string _rightmostPlayerAxisName;
     [Header("Max. points before reset")]
     [SerializeField] private int _maxPoints;
 
@@ -25,6 +30,9 @@ public class SO_GameplayParameters : ScriptableObject
         BallSpeed = () => _ballSpeed;
         BallSpeedMultiplier = () => Mathf.Abs(_ballSpeedMultiplier);
         PlayerPaddleSpeed = () => Mathf.Abs(_playerPaddleSpeed);
+        LeftmostPlayerAxisName = () => _leftmostPlayerAxisName;
+        RightmostPlayerAxisName = () => _rightmostPlayerAxisName;
         MaxPoints = () => Mathf.Abs(_maxPoints);
+        
     }
 }
--- Assets/Scripts/PaddleMotionManagement/PlayerMotionControl.cs
+++ Assets/Scripts/PaddleMotionManagement/PlayerMotionControl.cs
@@ -3,8 +3,54 @@ using System.Collections.Generic;
 using UnityEngine;
 using UniRx;
 using System;
+using System.Linq;
 
 public class PlayerMotionControl : MonoBehaviour
 {
+    private CompositeDisposable _compositeDisposable;
 
+    [SerializeField] private SO_GameplayParameters _gameplayParameters;
+    [SerializeField] private Transform _leftmostPlayerTransform;
+    [SerializeField] private Transform _rightmostPlayerTransform;
+
+    private void Awake()
+    {
+        if (_compositeDisposable != null)
+        {
+            _compositeDisposable.Dispose();
+        }
+
+        _compositeDisposable = new CompositeDisposable();
+    }
+
+    private void Start()
+    {
+        ManageAxisInput(_leftmostPlayerTransform,
+            _gameplayParameters.PlayerPaddleSpeed.Invoke(),
+            () => Input.GetAxis(_gameplayParameters.LeftmostPlayerAxisName.Invoke()))
+            .AddTo(_compositeDisposable);
+        ManageAxisInput(_rightmostPlayerTransform,
+            _gameplayParameters.PlayerPaddleSpeed.Invoke(),
+            () => Input.GetAxis(_gameplayParameters.RightmostPlayerAxisName.Invoke()))
+            .AddTo(_compositeDisposable);
+    }
+
+    private IDisposable ManageAxisInput(Transform playerPaddle, float paddleSpeed, Func<float> AxisInput)
+    {
+        return Observable
+            .EveryUpdate()
+            .AsUnitObservable()
+            .Select<Unit, Action>(_ =>
+            {
+                var currentPos = playerPaddle.position;
+                var y = Mathf.Clamp(currentPos.y + AxisInput.Invoke() * paddleSpeed * Time.deltaTime, -4.5f, +4.5f);
+                return () => { playerPaddle.position = new Vector3(currentPos.x, y, 0); };
+            })
+            .Subscribe(action => action.Invoke());
+    }
+
+    private void OnDestroy()
+    {
+        _compositeDisposable.Dispose();
+    }
 }
