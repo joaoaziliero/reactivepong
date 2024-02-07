955de9d9a18f0176a3648e6eb942992ad7757ebd
joaoaziliero <146762748+joaoaziliero@users.noreply.github.com>
2024-02-07 18:38:56 -0300

    slightly refactor score manager from old dev branch

--- Assets/Scripts/ScoreManagement/ScoreManager.cs
+++ Assets/Scripts/ScoreManagement/ScoreManager.cs
@@ -9,5 +9,63 @@ using System.Linq;
 
 public class ScoreManager : MonoBehaviour
 {
+    private CompositeDisposable _compositeDisposable;
+    private Collider2D _collider;
 
+    [SerializeField] private SO_GameplayParameters _gameplayParameters;
+    [Header("Corresponding display field")]
+    [SerializeField] private TextMeshProUGUI _textField;
+    [Header("Opponent's display field")]
+    [SerializeField] private TextMeshProUGUI _opposingTextField;
+
+    private void Awake()
+    {
+        if (_compositeDisposable != null)
+        {
+            _compositeDisposable.Dispose();
+        }
+
+        _compositeDisposable = new CompositeDisposable();
+        _collider = GetComponent<Collider2D>();
+    }
+
+    private void Start()
+    {
+        ManageScore(_collider, _opposingTextField, _textField, _gameplayParameters.MaxPoints.Invoke())
+            .AddTo(_compositeDisposable);
+    }
+
+    private IDisposable ManageScore(Collider2D trigger, TextMeshProUGUI textField, TextMeshProUGUI opposingField, int maxPoints)
+    {
+        return trigger
+            .OnTriggerEnter2DAsObservable()
+            .Where(incomingTrigger => incomingTrigger.gameObject.CompareTag("Ball"))
+            .Select<Collider2D, Action>(collision =>
+            {
+                var score = int.Parse(textField.text);
+                var opposingScore = int.Parse(opposingField.text);
+
+                return (score, opposingScore) switch
+                {
+                    var tuple when tuple.score == maxPoints => () => ResetScore(textField, opposingField)
+                    ,
+                    var tuple when tuple.opposingScore == maxPoints => () => ResetScore(textField, opposingField)
+                    ,
+                    _ => () => UpdateScore(textField, score + 1)
+                    ,
+                };
+            })
+            .Subscribe(action => action.Invoke());
+    }
+
+    private readonly Action<TextMeshProUGUI, TextMeshProUGUI> ResetScore =
+        (textField, opposingField) => { textField.text = 1.ToString(); opposingField.text = 0.ToString(); };
+
+    private readonly Action<TextMeshProUGUI, int> UpdateScore =
+        (textField, newScore) => { textField.text = newScore.ToString(); };
+
+    private void OnDestroy()
+    {
+        _compositeDisposable.Dispose();
+    }
 }
