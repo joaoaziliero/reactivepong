269f7ff3b31a0cf6f3c2a240283da8128def7fea
joaoaziliero <146762748+joaoaziliero@users.noreply.github.com>
2024-02-07 00:20:20 -0300

    make paddle motion and clamping rely on physics

--- Assets/Scripts/PaddleMotionManagement/PlayerMotionControl.cs
+++ Assets/Scripts/PaddleMotionManagement/PlayerMotionControl.cs
@@ -10,8 +10,8 @@ public class PlayerMotionControl : MonoBehaviour
     private CompositeDisposable _compositeDisposable;
 
     [SerializeField] private SO_GameplayParameters _gameplayParameters;
-    [SerializeField] private Transform _leftmostPlayerTransform;
-    [SerializeField] private Transform _rightmostPlayerTransform;
+    [SerializeField] private Rigidbody2D _leftmostPaddleBody;
+    [SerializeField] private Rigidbody2D _rightmostPaddleBody;
 
     private void Awake()
     {
@@ -25,27 +25,22 @@ public class PlayerMotionControl : MonoBehaviour
 
     private void Start()
     {
-        ManageAxisInput(_leftmostPlayerTransform,
+        ManageAxisInput(_leftmostPaddleBody,
             _gameplayParameters.PlayerPaddleSpeed.Invoke(),
             Input.GetAxis, _gameplayParameters.LeftmostPlayerAxisName.Invoke(),
             _compositeDisposable);
-        ManageAxisInput(_rightmostPlayerTransform,
+        ManageAxisInput(_rightmostPaddleBody,
             _gameplayParameters.PlayerPaddleSpeed.Invoke(),
             Input.GetAxis, _gameplayParameters.RightmostPlayerAxisName.Invoke(),
             _compositeDisposable);
     }
 
-    private void ManageAxisInput(Transform playerPaddle, float paddleSpeed, Func<string, float> GetAxis, string AxisName, CompositeDisposable disposables)
+    private void ManageAxisInput(Rigidbody2D playerPaddle, float paddleSpeed, Func<string, float> GetAxis, string AxisName, CompositeDisposable disposables)
     {
         Observable.EveryUpdate().Subscribe(_ => GetAxis(AxisName)).AddTo(disposables);
 
-        GetAxis.ObserveEveryValueChanged(InputFunction => Time.deltaTime * InputFunction(AxisName))
-            .Select<float, Action>(processedInput =>
-            {
-                var currentPos = playerPaddle.position;
-                var y = Mathf.Clamp(currentPos.y + paddleSpeed * processedInput, -4.5f, +4.5f);
-                return () => { playerPaddle.position = new Vector3(currentPos.x, y, 0); };
-            })
+        GetAxis.ObserveEveryValueChanged(InputFunction => InputFunction(AxisName))
+            .Select<float, Action>(inputReading => () => { playerPaddle.velocity = inputReading * paddleSpeed * Vector2.up; })
             .Subscribe(action => action.Invoke()).AddTo(disposables);
     }
 
