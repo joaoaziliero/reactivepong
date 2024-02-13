2612b8161bb9c771b159077a5ad42aa498c0c172
joaoaziliero <146762748+joaoaziliero@users.noreply.github.com>
2024-02-13 11:48:50 -0300

    test restrictions on value attributions

--- Assets/Scripts/TestScripts/SO_Test.cs
+++ Assets/Scripts/TestScripts/SO_Test.cs
@@ -8,9 +8,9 @@ public class SO_Test : ScriptableObject
 {
     public Func<int> Number { get; }
     public Action<int> SetNumber { get; set; }
-    private Action<int> SetAndResetSetter() { return (num) => { _number = num; SetNumber = null; }; }
+    private Action<int> SetAndResetSetter() { return (num) => { if (_number == 0) _number = num; SetNumber = null; }; }
 
-    [SerializeField] private int _number;
+    [SerializeField] private int _number = 0;
     
     private SO_Test()
     {
